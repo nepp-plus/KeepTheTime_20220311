558e7543f1590aa537acb1deff13c9d8d68c627b
nepp.plus <nepp.plus@gmail.com>
2022-03-16 10:49:31 +0900

    서버에서 내 약속목록을 불러와서, ArrayList에 담아주기

--- app/src/main/java/com/neppplus/keepthetime_20220311/api/APIList.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/api/APIList.kt
@@ -68,5 +68,8 @@ interface APIList {
         @Field("longitude") lng: Double,
     ) : Call<BasicResponse>
 
+    @GET("/appointment")
+    fun getRequestAppointmentList() : Call<BasicResponse>
+
 
 }
