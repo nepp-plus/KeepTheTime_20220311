3a1f9334a8394affa62cbac3e77f4c009696a35f
nepp.plus <nepp.plus@gmail.com>
2022-03-16 14:52:23 +0900

    시작점 : 출발좌표, 마지막점 : 클릭한 목적지가 되도록 경로 추가

--- app/src/main/java/com/neppplus/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/EditAppointmentActivity.kt
@@ -284,6 +284,8 @@ class EditAppointmentActivity : BaseActivity() {
 //                           첫번째 경로를 지나는 모든 정거장들의 위경도값을 담을 목록
                             val stationLatLngList = ArrayList<LatLng>()
 
+//                            출발지 좌표를 정거장 목록에 먼저 추가.
+                            stationLatLngList.add(coord)
 
 //                            불광~강남 :  도보5분 / 지하철30분 / 버스 30분 / 도보 5분
                             val subPathArr = firstPathObj.getJSONArray("subPath")
@@ -317,6 +319,9 @@ class EditAppointmentActivity : BaseActivity() {
                                 }
 
                             }
+
+//                            최종 정거장 ~ 도착지 까지 직선
+                            stationLatLngList.add( latLng )
 
 //                            완성된 정거장 경로들을 => path 의 경로로 재설정. 지도에 새로 반영.
 
