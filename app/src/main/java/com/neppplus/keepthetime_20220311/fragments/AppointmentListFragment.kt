8ae785b15632121bd4d5eaddbcaed34b98d09abf
nepp.plus <nepp.plus@gmail.com>
2022-03-16 11:34:04 +0900

    약속 목록  프래그먼트로 돌아오면, 서버에서 일정 목록 새로 받아서 표시  - 등록 완료시 자동 반영

--- app/src/main/java/com/neppplus/keepthetime_20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/fragments/AppointmentListFragment.kt
@@ -53,12 +53,17 @@ class AppointmentListFragment : BaseFragment() {
 
     override fun setValues() {
 
-        getMyAppointmentListFromServer()
 
         mAppointmentAdapter = AppointmentRecyclerAdapter(mContext, mAppointmentList)
         binding.appointmentRecyclerView.adapter = mAppointmentAdapter
         binding.appointmentRecyclerView.layoutManager = LinearLayoutManager(mContext)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        getMyAppointmentListFromServer()
     }
 
     fun getMyAppointmentListFromServer() {
@@ -67,6 +72,10 @@ class AppointmentListFragment : BaseFragment() {
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
 
                 if (response.isSuccessful) {
+
+//                    기존의 약속목록을 비우고 나서 추가
+                    mAppointmentList.clear()
+
                     val br = response.body()!!
 
                     mAppointmentList.addAll( br.data.appointments )
