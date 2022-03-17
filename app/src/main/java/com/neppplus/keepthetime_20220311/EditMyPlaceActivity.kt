90afc47bd167c6348715011aaa5478031066cb16
nepp.plus <nepp.plus@gmail.com>
2022-03-17 11:13:30 +0900

    네이버 지도의 마커 설정 기능 활용 예시

--- app/src/main/java/com/neppplus/keepthetime_20220311/EditMyPlaceActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/EditMyPlaceActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 import com.neppplus.keepthetime_20220311.databinding.ActivityEditMyPlaceBinding
 
 class EditMyPlaceActivity : BaseActivity() {
@@ -38,6 +39,7 @@ class EditMyPlaceActivity : BaseActivity() {
 //                    처음으로 지도를 클릭 한 상황
 //                    마커를 새로 만들어주자. => 위치정보는? latLng변수가 대입될 예정. 새로 만들 필요 없다.
                     marker = Marker()
+                    marker!!.icon = OverlayImage.fromResource(R.drawable.red_marker_icon)
                 }
 
                 mSelectedPoint = latLng
