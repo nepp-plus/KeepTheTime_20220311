4224e2ebb8c4c44ee978be1dae5f7abc28fcda58
nepp.plus <nepp.plus@gmail.com>
2022-03-23 10:37:31 +0900

    사진 선택하기전에, 파일 조회 권한 획득  - 파일 조회 : 코드로 해당 파일의 실제 경로 획득. (서버 첨부 OK)

--- app/src/main/java/com/neppplus/keepthetime_20220311/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.keepthetime_20220311.fragments
 
+import android.Manifest
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
@@ -12,6 +13,8 @@ import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.keepthetime_20220311.ManageMyFriendsActivity
 import com.neppplus.keepthetime_20220311.ManagePlacesActivity
 import com.neppplus.keepthetime_20220311.R
@@ -39,7 +42,7 @@ class MyProfileFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate( inflater, R.layout.fragment_my_profile, container, false )
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_profile, container, false)
         return binding.root
     }
 
@@ -53,14 +56,32 @@ class MyProfileFragment : BaseFragment() {
 
         binding.imgProfile.setOnClickListener {
 
+//            이미지 조회 권한 확인
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
 //            이미지 선택 화면으로 이동 - 안드로이드 제공 화면 활용 : Intent (4)
 
 //            다른 화면에서 결과 받아오기 - Intent (3) : startActivityForResult
 
-            val myIntent = Intent()
-            myIntent.action = Intent.ACTION_PICK // 뭔가 가지러 가는 행동이라고 명시.
-            myIntent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE // 사진을 가지러 간다고 명시.
-            startActivityForResult( myIntent, REQ_CODE_GALLERY )
+                    val myIntent = Intent()
+                    myIntent.action = Intent.ACTION_PICK // 뭔가 가지러 가는 행동이라고 명시.
+                    myIntent.type =
+                        android.provider.MediaStore.Images.Media.CONTENT_TYPE // 사진을 가지러 간다고 명시.
+                    startActivityForResult(myIntent, REQ_CODE_GALLERY)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "갤러리 조회 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener( pl )
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check()
 
         }
 
@@ -91,7 +112,8 @@ class MyProfileFragment : BaseFragment() {
 //                    로딩화면으로 복귀 (구글링 활용)
                     val myIntent = Intent(mContext, SplashActivity::class.java)
 
-                    myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    myIntent.flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 
                     startActivity(myIntent)
 
@@ -109,16 +131,17 @@ class MyProfileFragment : BaseFragment() {
     override fun setValues() {
 
 //        내 정보 조회 > UI 반영
-        apiList.getRequestMyInfo(  ).enqueue( object : Callback<BasicResponse> {
+        apiList.getRequestMyInfo().enqueue(object : Callback<BasicResponse> {
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
 
                 if (response.isSuccessful) {
 
                     val br = response.body()!!
 
-                    binding.txtNickname.text = br.data.user.nick_name   // 프래그먼트의 txtNickname은 어떻게 가져와야하는가?
+                    binding.txtNickname.text =
+                        br.data.user.nick_name   // 프래그먼트의 txtNickname은 어떻게 가져와야하는가?
 
-                    Glide.with(mContext).load(br.data.user.profile_img).into( binding.imgProfile )
+                    Glide.with(mContext).load(br.data.user.profile_img).into(binding.imgProfile)
 
                 }
 
@@ -144,14 +167,15 @@ class MyProfileFragment : BaseFragment() {
                 val selectedImageUri = data?.data!!  // 선택한 사진에 찾아갈 경로 (Uri) 받아내기
 
 //                Uri -> 실제 첨부 가능한 파일 형태로 변환. (File객체를, Path를 통해 만든다.) -> Retrofit에 첨부할 수 있게 됨.
-                val file = File( URIPathHelper().getPath(mContext, selectedImageUri) )
+                val file = File(URIPathHelper().getPath(mContext, selectedImageUri))
 
 //                완성된 파일을, Retrofit에 첨부 가능한 RequestBody 형태로 가공.
-                val fileReqBody = RequestBody.create( MediaType.get("image/*"),  file )
+                val fileReqBody = RequestBody.create(MediaType.get("image/*"), file)
 
 //                실제로 첨부하자. 일반 형태의 통신 X,  Multipart 형태로 전송. MultipartBody 형태로 2차 가공.
 //                cf) 파일이 같이 첨부되는 API통신은, Multipart 형태로 모든 데이터를 첨부해야함.
-                val multiPartBody = MultipartBody.Part.createFormData("profile_image", "myProfile.jpg", fileReqBody)
+                val multiPartBody =
+                    MultipartBody.Part.createFormData("profile_image", "myProfile.jpg", fileReqBody)
 
 //                실제 서버에 완성된 데이터 전송.
                 apiList.putRequestProfileImg(
